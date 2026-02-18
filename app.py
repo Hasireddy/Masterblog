bf0b7c8301493982fd9ed80109ff6a39bc17efe1
Hasireddy <malikireddy.uma@gmail.com>
2026-02-18 15:33:53 +0100

    Function to add new blog

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request
+from flask import Flask, render_template, request, redirect, url_for
 import json
 
 app = Flask(__name__)
@@ -12,10 +12,28 @@ def index():
 
 
 
+
 @app.route('/add', methods=['GET', 'POST'])
 def add_blog():
+    #Read data rom the file
+    with open("data.json", "r") as fileobj:
+        blogs = json.loads(fileobj.read())
+
     if request.method == 'POST':
-        pass
+        blog = {
+            "id": request.form.get("id"),
+            "title": request.form.get("title"),
+            "author": request.form.get("author"),
+            "content": request.form.get("content")
+        }
+
+        blogs.append(blog)
+
+        #Write data to the file
+        with open("data.json", "w") as fileobj:
+            json.dump(blogs, fileobj, indent=4)
+
+        return redirect(url_for('index'))
     return render_template("add.html")
 
 
