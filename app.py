8f223bc1e010b7a9bb5015bf218301f5505fc5a3
Hasireddy <malikireddy.uma@gmail.com>
2026-02-18 14:38:26 +0100

    rendering blogs with HTML template

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask,render_template
+import json
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    with open("data.json", "r") as fileobj:
+        blogs = json.loads(fileobj.read())
+        return render_template("index.html", blogs=blogs)
+
+
 
 
 if __name__ == '__main__':
